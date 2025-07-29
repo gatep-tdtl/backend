8c0f58d255154491398e1c29f9c1dc3900e1aa80
prathamesh hendre <prathameshhendre1252@gmail.com>
2025-07-29 10:29:18 +0530

    cors settings changed

--- gatep_platform_backend/gatep_platform_config/settings.py
+++ gatep_platform_backend/gatep_platform_config/settings.py
@@ -203,6 +203,11 @@ SIMPLE_JWT = {
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://your-production-domain.com",
+]
 
 CORS_ALLOW_CREDENTIALS = True
