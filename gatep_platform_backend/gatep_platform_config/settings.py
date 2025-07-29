43e83f37c19bbadf3ddf2147ddfab2bb0a4f62dd
prathamesh hendre <prathameshhendre1252@gmail.com>
2025-07-29 11:03:04 +0530

    changes

--- gatep_platform_backend/gatep_platform_config/settings.py
+++ gatep_platform_backend/gatep_platform_config/settings.py
@@ -207,6 +207,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "http://your-production-domain.com",
 ]
 
