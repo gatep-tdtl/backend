9c3e52b9e574646a55a36989c8d97531e090859b
prathamesh hendre <prathameshhendre1252@gmail.com>
2025-08-26 12:39:40 +0530

    pushed

--- gatep_platform_backend/gatep_platform_config/settings.py
+++ gatep_platform_backend/gatep_platform_config/settings.py
@@ -78,13 +78,27 @@ WSGI_APPLICATION = 'gatep_platform_config.wsgi.application'
 
 
 #Database - Using user's provided values from the image and explicit request
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'gatep_platform_db',
+#         'USER': 'dbmasteruser',
+#         'PASSWORD': 'database9014',
+#         'HOST': 'ls-f8259bafe38561c18d0d411f37aefbfabc0ff7bf.citdgny2wnek.ap-south-1.rds.amazonaws.com',  # or your database host
+#         'PORT': '3306',# or your database port
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION'"
+#         },
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gatep_platform_db',
-        'USER': 'dbmasteruser',
-        'PASSWORD': 'database9014',
-        'HOST': 'ls-f8259bafe38561c18d0d411f37aefbfabc0ff7bf.citdgny2wnek.ap-south-1.rds.amazonaws.com',  # or your database host
+        'USER': 'gatep_platform_db_user',
+        'PASSWORD': 'BR@jBvsKOd@C',
+        'HOST': '192.168.0.253',  # or your database host
         'PORT': '3306',# or your database port
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION'"
